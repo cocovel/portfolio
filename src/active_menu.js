9e08e88d0ed273a079771f1fc394ccb6a3326612
Cocovel <46144174+cocovel@users.noreply.github.com>
2023-10-31 20:59:17 -0500

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -1,21 +1,17 @@
 'use strict';
 
-// implementation plan
-// 1. Bring all section elements and menu items (queyrselector)
-// 2. Observe all sections using IntersectionObserver
-// 3. Activate a menu item which belongs to the section observed
-// section being observed: 
-// - If several sections are observed, the first section is selected.
-// - If the contact section is observed, the last section is selected.
-
 const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonial', '#contact'];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach(section => observer.observe(section));
 
@@ -27,22 +23,26 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
   });
-  console.log(visibleSections);
-  console.log('무조건 라스트 섹션', selectLastOne);
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
-  console.log(sectionIds[navIndex]);
-
-  function findFirstIntersecting(intersections) {
-    const index = intersections.indexOf(true);
-    return index > 0 ? index : 0
-
-  }
+  selectNavItem(navIndex);
 }
 
+function findFirstIntersecting(intersections) {
+  const index = intersections.indexOf(true);
+  return index > 0 ? index : 0
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
+}
 
 
